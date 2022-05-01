1b75ab36fe232ce2976f84de3e4d0e676c45bbfc
vladimirzhinev <vladimirzhinev@gmail.com>
2022-05-01 15:05:17 +0300

    add solves of queue removeFL stack

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,24 +13,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+
 class Queue {
+   constructor() {
+      this.head = null
+   }
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+   getUnderlyingList() {
+      return this.head
+   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+   enqueue(value) {
+      if (this.head == null) {
+         this.head = new ListNode(value)
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+      } else {
+         let currentNode = this.head
+         while (currentNode.next) {
+            currentNode = currentNode.next
+         }
+         currentNode.next = new ListNode(value)
+         this.tail = currentNode.next
+      }
+   }
+
+   dequeue() {
+      const valueDequeue = this.head.value
+      this.head = this.head.next
+      return valueDequeue
+   }
 }
 
 module.exports = {
-  Queue
+   Queue
 };
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+
+   let current = l;
+
+   while (l.value === k) {
+      l = l.next;
+   }
+
+
+   while (current.next) {
+      if (current.next.value == k) {
+         current.next = current.next.next
+      }
+   }
+
+
+   return l;
+
+
+
 }
 
 module.exports = {
-  removeKFromList
+   removeKFromList
 };
